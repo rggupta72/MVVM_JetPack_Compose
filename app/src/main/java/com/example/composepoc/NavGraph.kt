eacb62845009f6d29d4b67349ace4c1ef86abf94
Raju Gupta <rggupta72@gmail.com>
2025-06-26 19:39:20 +0530

    Nav Graph sample added

---
+++ app/src/main/java/com/example/composepoc/NavGraph.kt
@@ -0,0 +1,74 @@
+package com.example.composepoc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+class NavGraph : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavController()
+        }
+    }
+}
+
+@Composable
+fun NavController() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "registration") {
+        composable(route = "registration") {
+            RegistrationScreen() {
+                navController.navigate("login")
+            }
+        }
+        composable(route = "login") {
+            LoginScreen() { abc, xyz ->
+                navController.navigate("main/${abc}/${xyz}")
+            }
+        }
+        composable(route = "main/{email}/{password}", arguments = listOf(
+            navArgument(name = "email") {
+                type = NavType.StringType
+            },
+            navArgument(name = "password") {
+                type = NavType.StringType
+            }
+        )) {
+            val email = it.arguments?.getString("email") ?: ""
+            MainScreen(email) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun RegistrationScreen(onClick: () -> Unit) {
+    Text("Registration", style = MaterialTheme.typography.headlineLarge,
+        modifier = Modifier.clickable { onClick() })
+}
+
+@Composable
+fun LoginScreen(onClick: (abc: String, xyz: String) -> Unit) {
+    Text("Login", style = MaterialTheme.typography.headlineLarge,
+        modifier = Modifier.clickable { onClick("tester", "tester1") })
+}
+
+@Composable
+fun MainScreen(email: String, onClick: () -> Unit) {
+    Text(email, style = MaterialTheme.typography.headlineLarge,
+        modifier = Modifier.clickable { onClick() })
+}
