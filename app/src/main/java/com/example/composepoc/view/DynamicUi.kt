ac4627645805a8dff6e263fb871523498fb966ce
Raju Gupta <rggupta72@gmail.com>
2025-07-09 00:59:46 +0530

    Dynamic Card added

--- app/src/main/java/com/example/composepoc/view/DynamicUi.kt
+++ app/src/main/java/com/example/composepoc/view/DynamicUi.kt
@@ -1,6 +1,7 @@
 package com.example.composepoc.view
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -22,82 +23,88 @@ fun DynamiCUi() {
 
     val textvalue = remember { mutableStateOf("Raje") }
 
-    ConstraintLayout {
-        val (headingText, mediumStartText, mediumEndText, button, textField) = createRefs()
-        val startGuideLine = createGuidelineFromStart(10.dp)
-        val endGuideLine = createGuidelineFromEnd(10.dp)
-        val topGuideLine = createGuidelineFromTop(10.dp)
-        val centerGuideline = createGuidelineFromStart(0.5f)
-        val topBarrier = createBottomBarrier(mediumStartText, mediumEndText)
+    CardWithText(
+        modifier = Modifier.padding(10.dp),
+        elevation = 5.dp) {
+        ConstraintLayout {
+            val (headingText, mediumStartText, mediumEndText, button, textField) = createRefs()
+            val startGuideLine = createGuidelineFromStart(10.dp)
+            val endGuideLine = createGuidelineFromEnd(10.dp)
+            val topGuideLine = createGuidelineFromTop(10.dp)
+            val startGuideline = createGuidelineFromStart(0.5f)
+            val bottomBarrier = createBottomBarrier(mediumStartText, mediumEndText)
 //        createHorizontalChain(
 //            mediumStartText, mediumEndText,
 //            chainStyle = ChainStyle.SpreadInside
 //        )
 
-        HeadingText(
-            modifier = Modifier
-                .fillMaxWidth()
-                .constrainAs(headingText) {
-                    start.linkTo(startGuideLine)
-                    top.linkTo(topGuideLine)
-                    end.linkTo(endGuideLine)
-                }
-                .semantics { heading() }
-                .semantics { contentDescription = "Dynamic Text" },
-            "Dynamic Text",
-            Color.Black,
-            MaterialTheme.typography.headlineLarge,
-            TextAlign.Center
-        )
 
-        MediumText(
-            modifier = Modifier
-                .constrainAs(mediumStartText) {
-                    top.linkTo(headingText.bottom)
+            HeadingText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(headingText) {
+                        start.linkTo(startGuideLine)
+                        top.linkTo(topGuideLine)
+                        end.linkTo(endGuideLine)
+                    }
+                    .semantics { heading() }
+                    .semantics { contentDescription = "Dynamic Text" },
+                "Dynamic Text",
+                Color.Black,
+                MaterialTheme.typography.headlineLarge,
+                TextAlign.Center
+            )
+
+            MediumText(
+                modifier = Modifier
+                    .constrainAs(mediumStartText) {
+                        top.linkTo(headingText.bottom)
+                        start.linkTo(parent.start, 10.dp)
+                        end.linkTo(startGuideline)
+                        width = Dimension.fillToConstraints
+                    },
+                "Hi My Name is Raju Gupta", Color.Blue, textAlign = TextAlign.Center,
+            )
+
+            MediumText(
+                modifier = Modifier
+                    .constrainAs(mediumEndText) {
+                        top.linkTo(headingText.bottom)
+                        start.linkTo(startGuideline, 10.dp)
+                        end.linkTo(parent.end, 10.dp)
+                        width = Dimension.fillToConstraints
+                    },
+                "I am android developer with 10 plus years of experiance",
+                Color.Blue,
+            )
+
+            ButtonUi(
+                modifier = Modifier
+                    .constrainAs(button) {
+                        top.linkTo(bottomBarrier)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .semantics { contentDescription = "Button" },
+                color = ButtonDefaults.buttonColors(Color.Red),
+                "Button",
+                textColour = Color.White,
+                textAlign = TextAlign.Start
+            ) {
+
+            }
+
+            TextInputField(modifier = Modifier
+                .constrainAs(textField) {
+                    top.linkTo(button.bottom)
                     start.linkTo(parent.start, 10.dp)
-                    end.linkTo(centerGuideline)
-                    width = Dimension.fillToConstraints
-                },
-            "Hi My Name is Raju Gupta", Color.Blue, textAlign = TextAlign.Center,
-        )
-
-        MediumText(
-            modifier = Modifier
-                .constrainAs(mediumEndText) {
-                    top.linkTo(headingText.bottom)
-                    start.linkTo(centerGuideline, 10.dp)
                     end.linkTo(parent.end, 10.dp)
                     width = Dimension.fillToConstraints
-                },
-            "I am android developer with 10 plus years of experiance",
-            Color.Blue,
-        )
-
-        ButtonUi(
-            modifier = Modifier
-                .constrainAs(button) {
-                    top.linkTo(topBarrier)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-                .semantics { contentDescription = "Button" },
-            color = ButtonDefaults.buttonColors(Color.Red),
-            "Button",
-            textColour = Color.White,
-            textAlign = TextAlign.Start
-        ) {
-
-        }
-
-        TextInputField(modifier = Modifier
-            .constrainAs(textField) {
-                top.linkTo(button.bottom)
-                start.linkTo(parent.start, 10.dp)
-                end.linkTo(parent.end, 10.dp)
-                width = Dimension.fillToConstraints
+                }.padding(vertical = 10.dp)
+                .semantics { contentDescription = textvalue.value }, textvalue.value
+            ) {
+                textvalue.value = it
             }
-            .semantics { contentDescription = textvalue.value }, textvalue.value) {
-            textvalue.value = it
         }
 
 
